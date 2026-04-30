3a492d43a88f0ae781652bdf434b69cb4bdeab41
Ibtisam Ishak <ibtisamishak25@gmail.com>
2026-04-30 14:51:48 +0800

    Refactor SQL queries for COVID-19 analysis
    Updated SQL queries for COVID-19 data analysis, including mortality and vaccination rates.

--- Covid Data Exploratory SQL.sql
+++ Covid Data Exploratory SQL.sql
@@ -1,11 +1,11 @@
---Lets scan through covid deaths table
+--View the covid_deaths table
 
 SELECT *
 FROM portfolio_project..covid_deaths
 WHERE continent is not NULL
 ORDER BY 3,4
 
---Select data that we are going to use
+--Select columns that we are going to use
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM portfolio_project..covid_deaths
@@ -13,16 +13,16 @@ WHERE continent is not NULL
 ORDER BY 1,2
 
 --Looking at Total Cases vs Total Deaths
---Shows the likelihood of dying if you contract covid in your country
+--Shows the mortality rate of COVID-19 in your country
 
-SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as mortality_rate
 FROM portfolio_project..covid_deaths
 WHERE location = 'Malaysia'
 AND continent is not NULL
 ORDER BY date
 
 --Looking at Total Cases vs Population
---Shows what percentage of population contracted covid
+--Shows what percentage of the population contracted COVID
 
 SELECT location, date, total_cases, population, (total_cases/population)*100 as percent_population_infected
 FROM portfolio_project..covid_deaths
@@ -38,7 +38,7 @@ WHERE continent is not NULL
 GROUP BY location, population
 ORDER BY percent_population_infected DESC
 
---Showing Countries with Highest Death Count per Population
+--Showing Countries with the Highest Death Count per Population
 
 SELECT location, MAX(cast(total_deaths as int)) as total_death_count
 FROM portfolio_project..covid_deaths
@@ -48,7 +48,7 @@ ORDER BY total_death_count DESC
 
 --Let's break things down by Continent
 
---Showing Continents with Highest Death Count per Population
+--Showing Continents with the Highest Death Count per Population
 
 SELECT continent, MAX(cast(total_deaths as int)) as total_death_count
 FROM portfolio_project..covid_deaths
@@ -58,20 +58,21 @@ ORDER BY total_death_count DESC
 
 --Global numbers by date
 
-SELECT date, SUM(new_cases) as total_new_cases, SUM(cast(new_deaths as int)) as total_new_deaths
-, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 as death_percentage
+SELECT date, SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths
+, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 as mortality_rate
 FROM portfolio_project..covid_deaths
 WHERE continent is not NULL
 GROUP BY date
 ORDER BY 1, 2
 
 --Total Global numbers 
-SELECT SUM(new_cases) as total_new_cases, SUM(cast(new_deaths as int)) as total_new_deaths, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 as death_percentage
+SELECT SUM(new_cases) as total_new_cases, SUM(cast(new_deaths as int)) as total_new_deaths
+, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 as mortality_rate
 FROM portfolio_project..covid_deaths
 WHERE continent is not NULL
 ORDER BY 1, 2
 
---Lets scan through covid vaccinations table
+--View the covid_vaccinations table
 SELECT *
 FROM portfolio_project..covid_vaccinations
 ORDER BY 3,4
@@ -101,7 +102,7 @@ With PopvsVac (Continent, location, date, population, new_vaccinations, rolling_
 (
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location
-, dea.date) as rolling_people_vaccinated --adds up new vaccinations with the numbers from the day before
+, dea.date) as rolling_people_vaccinated 
 FROM portfolio_project..covid_deaths dea
 JOIN portfolio_project..covid_vaccinations vac
 	ON dea.location = vac.location
@@ -127,7 +128,7 @@ Rolling_people_vaccinated numeric
 INSERT INTO #percent_population_vaccinated
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location
-, dea.date) as rolling_people_vaccinated --adds up new vaccinations with the numbers from the day before
+, dea.date) as rolling_people_vaccinated 
 FROM portfolio_project..covid_deaths dea
 JOIN portfolio_project..covid_vaccinations vac
 	ON dea.location = vac.location
@@ -137,7 +138,7 @@ WHERE dea.continent is not null
 SELECT *, (rolling_people_vaccinated/population)*100 as vaccinated_percentage
 FROM #percent_population_vaccinated
 
---Creating view to store data for later visualization
+--Creating a view to store data for later visualization
 
 CREATE VIEW percent_population_vaccinated as
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
